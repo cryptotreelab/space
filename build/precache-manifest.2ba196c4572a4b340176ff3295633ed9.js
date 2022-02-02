eb3ae5d87bffaa8966e1b32889f64f16bc69adb0
cryptotreelab <82641981+cryptotreelab@users.noreply.github.com>
2022-02-02 16:44:02 +0330

    Add files via upload

---
+++ build/precache-manifest.2ba196c4572a4b340176ff3295633ed9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7f7e720b2fd88bbf3fe4b0516f5e58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f280dc010463fb9de1",
+    "url": "/static/css/main.9136e423.chunk.css"
+  },
+  {
+    "revision": "2ec8d3f0bd424eaf9ffb",
+    "url": "/static/js/2.c6f52576.chunk.js"
+  },
+  {
+    "revision": "157328740c8dda20039f1118164cf903",
+    "url": "/static/js/2.c6f52576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ef5b1d45d75d6d52b0",
+    "url": "/static/js/3.c2e4e81f.chunk.js"
+  },
+  {
+    "revision": "13f280dc010463fb9de1",
+    "url": "/static/js/main.6f18476c.chunk.js"
+  },
+  {
+    "revision": "4b3d881ade7bed29fb91",
+    "url": "/static/js/runtime-main.b70de16e.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
